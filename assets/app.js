47830cf7bf23e1f2bf58a385f2e605df1bfc79a7
maya.pessel <maya.pessel@gmail.com>
2021-12-14 15:53:02 +0100

    Little styling of img thumbnail with scss and some bootstrap classes

--- assets/app.js
+++ assets/app.js
@@ -19,6 +19,3 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-}); 
